dda17ab4131d146265b401d7ebb9e73a23f58709
Djiby_Seck4 <djibysec4@gmail.com>
2020-09-13 20:27:55 +0000

    first commit

---
+++ src/test/java/sn/gs/demo/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.gs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
